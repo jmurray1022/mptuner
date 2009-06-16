6f6e99848424146931f4e071449eda3bf820a8bc
Jared <jared@jared-laptop.(none)>
2009-06-16 10:16:59 -0400

    - Added fit and sequential filtering - Removed running option, for the time being - Fixed MP to ignore NaN - NaN was being pushed into the polish results - *Lots* of code cleanup - Probably other stuff, it's been way too long since the last commit.

--- datadef.py
+++ datadef.py
@@ -1,110 +1,157 @@
 import csv
+from time import time
+
 import wx
-from support import *
 import numpy as np
 import matplotlib.numerix.ma as ma
-from time import time
+
+from support import *
 
 class DataObj:
     def __init__(self, fname):
-        self.fname=fname
-        self.ind=array([])
-        self.data=array([])
-        self.colnames=array([])
-        self.subset=array([])
-        self.cols=array([])
-        self.colsettings=array([])
-        self.factors=[]
-        self.settings=[]
-        self.groups=[]
-        self.groupnames=[]
-        self.group_cols=[]
-        self.groupon=[]
+        self.fname = fname
+        self.ind = array([])
+        self.data = array([])
+        self.colnames = array([])
+        self.subset = array([])
+        self.cols = array([])
+        self.colsettings = array([])
+        self.factors = []
+        self.settings = []
+        self.groups = []
+        self.groupnames = []
+        self.group_cols = []
+        self.groupon = []
         
         self.read_data()
         self.init_masks()
     
     def read_data(self):
-        '''Read the data in self.fname. Self.colnames contains column names, self.data has the data as floats.
+        '''
+        Read the data in self.fname. Self.colnames contains column names, 
+        self.data has the data as floats.
         '''
         
-        file=open(self.fname)
-        line=file.readline()
-        if line[-1]=='\n':
-            line=line.rstrip()
-        if line[0]=='"':
-            line=line.replace('"','')
-        cn=line.rsplit(",")
-        self.indname = cn[0]
-        self.colnames=np.array(cn[1:len(cn)])
-        nc=len(self.colnames)
-        self.colsettings=np.vstack([name.rsplit(".")] for name in self.colnames)
-        #self.settings
-
         def missing_data(x):
+            '''
+            Substitute NumPy's nan for missing data
+            '''
             if x == 'NaN' or x=='NA':
                 return np.nan
             else:
                 return float(x)
-        conv = dict( (k, missing_data) for k in range(nc+1) )
-        df = np.loadtxt(self.fname, delimiter = ',', converters = conv, skiprows=1)
-        self.ind=df[:,0]
-        self.data=df[:,1:len(df[0,])]
+        
+        file = open(self.fname)
+        line = file.readline()
+        if line[-1] == '\n':
+            line = line.rstrip()
+        if line[0] == '"':
+            line = line.replace('"','')
+        cn = line.rsplit(",")
+        self.indname  =  cn[0]
+        self.colnames = np.array(cn[1:len(cn)])
+        nc = len(self.colnames)
+        self.colsettings = np.vstack([name.rsplit(".")] for name in self.colnames)
+        
+        conv = dict((k, missing_data) for k in range(nc+1))
+        df = np.loadtxt(self.fname, delimiter=',', converters=conv, skiprows=1)
+        self.ind = df[:,0]
+        self.data = df[:,1:len(df[0,])]
         del df
         #self.data=ma.masked_invalid(df[:,1:len(df[0,])])
         
         self.factors = map(str,range(len(self.colsettings[0])))
-        self.settings = dict((self.factors[i], sorted(list(set(self.colsettings[:,i])), ncmp)) for i in range(len(self.factors)))
+        self.settings = dict((self.factors[i], \
+            sorted(list(set(self.colsettings[:,i])), ncmp)) \
+            for i in range(len(self.factors)))
         
     def init_masks(self):
         self.resmask = ma.make_mask_none(self.data.shape)
         self.colmask = ma.make_mask_none(self.data.shape)
-    
-    def do_seq_polish(self, frame, cols=array([]), cs=1.5, ce=3.0, eps=0.01, maxiter=100, repolish=True):
-        '''Do the sequential median polish. Build two masks, one for coleff and one for reseff.
-        Required: 
-        frame, int representing frame size
-        cols, numpy array with column indices
-        '''
-        shape=self.data.shape
-        nrow=shape[0]
-        ncol=shape[1]
-
-        if cols==array([]):
-            cols=arange(0,ncol)
-            
-        numframe=nrow/frame
-        r=nrow%frame
+        self.incmask = ma.make_mask_none(self.data.shape)
+        self.fitmask = ma.make_mask_none(self.data.shape)
+        self.masks = (self.resmask, self.colmask, self.incmask, self.fitmask)
+        self.masknames = ("resmask", "colmask", "incmask", "fitmask")
         
-        print r; print numframe
+    def mask(self):
+        out = ma.make_mask_none(self.data.shape)
+        for mask in self.masks: #(self.resmask, self.colmask, self.incmask, self.fitmask):
+            out = ma.mask_or(out, mask)
+        return out
+    
+    def do_seq_polish(self, frame, cols=array([]), cs=1.5, ce=3.0, 
+            fit = 0.0, seq = 0, eps=0.01, maxiter=100, 
+            repolish=True, save_polish=False):
+        
+        shape = self.data.shape
+        nrow = shape[0]
+        ncol = shape[1]
+
+        if cols == array([]):
+            cols = arange(0,ncol)
+            
+        numframe = nrow / frame
+        r = nrow % frame
+        
+        self.init_masks()
+        
+        if save_polish:
+            self.overall = np.zeros(self.data.shape)
+            self.coleff = np.zeros(self.data.shape)
+            self.roweff = np.zeros(self.data.shape)
+            self.residuals =  np.zeros(self.data.shape)
         
         #The data we skip due to frame size is all masked
         #self.colmask[0:r,:]=ones(self.colmask[0:r,:].shape,dtype=bool)
         #self.resmask[0:r,:]=ones(self.resmask[0:r,:].shape,dtype=bool)
         
-        dlg = wx.ProgressDialog("Median polish", "Performing median polish algorithm", 
-            maximum = numframe, style = wx.PD_APP_MODAL | wx.PD_SMOOTH | wx.PD_AUTO_HIDE)
+        dlg = wx.ProgressDialog("Median polish", 
+            "Performing median polish algorithm", maximum = numframe, 
+            style = wx.PD_APP_MODAL | wx.PD_SMOOTH | wx.PD_AUTO_HIDE)
   
         for i in range(numframe):
-            low=r+i*frame
-            high=r+(i+1)*frame
-            polish=medpolish(self.data[low:high,cols], eps=eps, maxiter=maxiter)
-            self.colmask[low:high,cols]=colclean(self.data[low:high,cols], polish['col'], cs)
-            self.resmask[low:high,cols]=resclean(self.data[low:high,cols], polish['res'], ce)
+            
+            low = i*frame    #r+i*frame
+            high = (i+1)*frame    #r+(i+1)*frame
+            
+            polish = medpolish(self.data[low:high,cols], eps=eps, 
+                maxiter=maxiter)
+            
+            if save_polish:
+                self.overall[low:high,cols] = polish['overall']
+                self.coleff[low:high,cols] = polish['col']
+                self.roweff[low:high,cols] = polish['row'][:,np.newaxis]
+                self.residuals[low:high,cols] = polish['res']
+            
+            if seq > 0:
+                self.incmask[low:high, cols] = seqclean(polish['res'] + \
+                    polish['row'].reshape(frame, 1), seq)
+            if fit > 0:
+                self.fitmask[low:high, cols] = fitclean(self.ind[low:high], 
+                    polish['res'], fit)
+            if cs > 0:
+                self.colmask[low:high, cols] = \
+                    colclean(self.data[low:high, cols], polish['col'], cs)
+            if ce > 0:
+                self.resmask[low:high, cols] = \
+                    resclean(self.data[low:high, cols], polish['res'], ce)
+                
             dlg.Update(i+1)
             
         dlg.Destroy()
     
-    def do_running_polish(self, lag, cols=array([]), cs=1.5, ce=3.0, eps=0.01, maxiter=100, repolish=True):
+    def do_running_polish(self, lag, cols=array([]), cs=1.5, ce=3.0, 
+            eps=0.01, maxiter=100, repolish=True):
+            
         nrow = self.data.shape[0]
-        lag = 7
         iter = nrow - 2 * lag
         
         if cols==array([]):
             cols=arange(0,ncol)
             
-        dlg = wx.ProgressDialog("Median polish", "Performing median polish algorithm", 
-            maximum = iter, style = wx.PD_APP_MODAL | wx.PD_SMOOTH | wx.PD_AUTO_HIDE)
+        dlg = wx.ProgressDialog("Median polish", 
+            "Performing median polish algorithm", maximum = iter, 
+            style = wx.PD_APP_MODAL | wx.PD_SMOOTH | wx.PD_AUTO_HIDE)
             
         #test = [i for i in range(iter)]
         #print test
@@ -121,23 +168,16 @@ class DataObj:
             low = i
             high = i + 2 * lag + 1
             start = time()
-            polish = medpolish(self.data[low:high,cols], eps=eps, maxiter=maxiter)
-            polishtime += time() - start
+            polish = medpolish(self.data[low:high,cols], eps=eps, 
+                maxiter=maxiter)
             start = time()
-            self.colmask[i+lag,cols] = rcolclean(self.data[low:high,cols], polish['col'], cs)
-            colcleantime += time() - start
-            self.resmask[i+lag,cols]=rresclean(self.data[low:high,cols], lag, polish['res'], ce)
+            self.colmask[i+lag,cols] = rcolclean(self.data[low:high,cols], 
+                polish['col'], cs)
+            self.resmask[i+lag,cols] = rresclean(self.data[low:high,cols], 
+                lag, polish['res'], ce)
             dlg.Update(i+1)
-        
-        #def f(frame):
-        #    self.colmask[frame[0]+lag,cols] = rcolclean(self.data[frame[0]:frame[1],cols], medpolish(self.data[frame[0]:frame[1],cols], eps=eps, maxiter=maxiter)['col'], cs)
-        #frames = [(i,i+2*lag+1) for i in range(iter)]
-        #map(f, frames)
-            
-        dlg.Destroy()
 
-        print "Polish time:"; print polishtime
-        print "Colclean time:"; print colcleantime
+        dlg.Destroy()
     
     def outdata(self):
         mask = ma.mask_or(self.colmask, self.resmask)
@@ -148,5 +188,20 @@ class DataObj:
             for j, col in enumerate(row):
                 if np.isnan(ldata[i][j]): ldata[i][j] = "NA"
                 
-        fulldata = vstack((hstack((array(self.indname),self.colnames)), hstack((transpose(np.atleast_2d(self.ind)), ldata))))
+        fulldata = vstack((hstack((array(self.indname),self.colnames)), 
+            hstack((transpose(np.atleast_2d(self.ind)), ldata))))
         return fulldata
+        
+    def outmask(self, mask):
+        intmask = np.array(mask, int)
+        out = vstack((hstack((array(self.indname),self.colnames)), 
+            hstack((transpose(np.atleast_2d(self.ind)), intmask))))
+        return out
+    
+    def outpolish(self, arr, fill_value = np.nan):
+        masked = ma.masked_invalid(arr)
+        masked.set_fill_value(fill_value)
+        cleaned = masked.filled()
+        out = vstack((hstack((array(self.indname),self.colnames)), 
+            hstack((transpose(np.atleast_2d(self.ind)), cleaned))))
+        return out
--- support.py
+++ support.py
@@ -1,193 +1,245 @@
+import csv
+
 from pylab import *
 import numpy as np
 import matplotlib
-#import matplotlib.dates as mdates
 import matplotlib.mlab as mlab
 import matplotlib.numerix.ma as ma
-import csv
+from scipy.stats import nanmedian
 
-#class Group
+def longest_contig_max(x):
+    '''
+    Returns the length of the longest monotonic sequence in x
+    '''
+    inc = longest_contig(x)
+    dec = longest_contig(x, inc = False)
+    return max(inc, dec)
+
+def longest_contig(x, inc = True):
+    '''
+    Returns the length of thelongest contiguous ordered 
+    (default is increasing) sequence.
+    Sequences don't have to be strictly monotonic.
+    '''
+    longest = 1
+    tmp = 1
+    switch = 0
+    if inc == False:
+        x = -1 * x
+    for i in range(x.size):
+        if i > 0:
+            if x[i] >= x[i-1]:
+                tmp = tmp + 1
+            else:
+                switch = 1
+                if tmp > longest:
+                    longest = tmp
+                tmp = 1
+    if switch == 0:
+        longest = tmp
+    return longest
 
 def ncmp(a, b):
     try:
-        x=float(a)
-        y=float(b)
+        x = float(a)
+        y = float(b)
     except ValueError:
-        x=a
-        y=b
-    if x>y:
+        x = a
+        y = b
+    if x > y:
        return 1
-    elif x==y:
+    elif x == y:
        return 0
     else: # x<y
        return -1
-
-
-###########################################################
-# Begin quantile function
-###########################################################
 
 def quantile(a, p):
     """Returns the pth quantile of a (a numpy array). Interpolation is 
     type 7 described in the R documentation, the default for R's quantile()
     """
-    x=a[where(~isnan(a))]
+    x = a[where(~isnan(a))]
     if not x.any():
         return np.nan
     else:
-        n=len(x)
+        n = len(x)
         x.sort()
-        k=p*(n-1)+1
-        i=int(k) #truncates k toward zero, gives us an index
-        g=k-i #the fractional part of k, used to weight the 2 closest order statistics
-        if i<n:
-            result=(1-g)*x[i-1]+g*x[i]
+        k = p * (n - 1) + 1
+        i = int(k)   #truncates k toward zero, gives us an index
+        g = k - i    #used to weight the 2 closest order statistics
+        if i < n:
+            result = (1 - g) * x[i - 1] + g * x[i]
         else:
-            result=x[i-1]
+            result = x[i - 1]
     return result
-
-###########################################################
-# End quantile function
-###########################################################
-
-###########################################################
-# Begin iqr (interquartile range) function
-###########################################################
 
 def iqr(x):
     '''Returns the interquartile range of a numpy array x.
     '''
-    result=quantile(x,0.75)-quantile(x,0.25)
+    result = quantile(x, 0.75) - quantile(x, 0.25)
     return result
 
-###########################################################
-# End iqr (interquartile range) function
-###########################################################
-
-###########################################################
-# Begin medpolish (median polish) function
-###########################################################
-
-def medpolish(z,eps=0.01,maxiter=10):
+def medpolish(x, eps=0.01, maxiter=10):
     '''Perform Tukey's median polish on z with precision eps.
     The algorithm will continue until convergence or until maxiter'''
-    #Initializing variables.
-    x=z.copy()
-    nr=x.shape[0]
-    nc=x.shape[1]
-    t=0
-    delta=0
-    r=np.zeros(nr)
-    c=np.zeros(nc)
-    rdelta=r
-    cdelta=c
-    oldsum=np.sum(x)
-    newsum=0
-    converged=0 #0==false
-    result="Error"
 
-    #begin polish loop
+    #x = z.copy()
+    nr = x.shape[0]
+    nc = x.shape[1]
+    
+    t = 0
+    delta = 0
+    
+    r = np.zeros(nr)
+    c = np.zeros(nc)
+    
+    rdelta = r
+    cdelta = c
+    
+    oldsum = np.sum(x)
+    newsum = 0
+    converged = False
+    result = "Error"
+    
+    # scipy's nanmedian is much slower than numpy's median (about 30 - 35%), 
+    # so only use it if we have to.
+    # TODO: check if this is still true after replacing median with np.median
+    # in scipy.stats (median is deprecated in favor of np.median)
+    nans = np.any(np.isnan(x))
+
     for i in range(1,maxiter):
 
-        #subtract the row medians from the rows of the matrix
-        #and add them to r
-        np.median(x,1)
-        x=x-rdelta[:,newaxis]
-        r=r+rdelta
+        # subtract the row medians from the rows of the matrix
+        # and add them to r
+        if nans:
+            rdelta = nanmedian(x, 1)
+        else:
+            rdelta = np.median(x, 1)
+        x = x - rdelta[:, newaxis]
+        r = r + rdelta
 
-        #subtract the median of r from r and add it to the overall effect
-        delta=np.median(r)
-        r=r-delta
-        t=t+delta
+        # subtract the median of r from r and add it to the overall effect
+        if nans:
+            delta = nanmedian(r)
+        else:
+            delta = np.median(r)
+        r = r - delta
+        t = t + delta
 
-        #subtract the column medians from the columns of the matrix
-        #and add them to c
-        cdelta = np.median(x,0)
-        x=x-cdelta
-        c=c+cdelta
+        # subtract the column medians from the columns of the matrix
+        # and add them to c
+        if nans:
+            cdelta = nanmedian(x,0)
+        else:
+            cdelta = np.median(x,0)
+        x = x - cdelta
+        c = c + cdelta
 
-        #subtract the median of c from c and add it to the overall effect
-        delta=np.median(c)
-        c=c-delta
-        t=t+delta
+        # subtract the median of c from c and add it to the overall effect
+        if nans:
+            delta = nanmedian(c)
+        else:
+            delta = np.median(c)
+        c = c - delta
+        t = t + delta
 
-        #sum of abs value of the elements
-        newsum=np.sum(np.abs(x[where(~isnan(x))]))
+        # sum of abs value of the elements
+        newsum = np.sum(np.abs(x[where(~isnan(x))]))
 
-        #the polish has converged if the abs sum of the residual matrix
-        #is arbitrarily small (or zero)
-        converged=abs(newsum-oldsum)<eps*newsum or newsum==0
+        # the polish has converged if the abs sum of the residual matrix
+        # is arbitrarily small (or zero)
+        converged = abs(newsum - oldsum) < eps * newsum or newsum == 0
 
         if converged:
             break
 
-        oldsum=newsum
+        oldsum = newsum
     #End polish loop
 
     if converged:
         #make our results
-        result={"iter":i-1, "overall":t, "row":r, "col":c, "res":x}
+        result = {"iter":i-1, "overall":t, "row":r, "col":c, "res":x}
     else:
         print "median polish failed to converge. increase maxiter?"
         #result={"iter":i-1, "overall":t, "row":r, "col":c, "res":x}
     return result
 
-##########################################################################
-# End medpolish function
-##########################################################################
-
-##########################################################################
-# Begin colclean (column effect clean) function
-##########################################################################
-
 def colclean(x, col_eff, cs):
-    iqrange=iqr(col_eff)
-    low=quantile(col_eff,0.25)-cs*iqrange
-    high=quantile(col_eff,0.75)+cs*iqrange
-    colkill=where((col_eff<low)|(col_eff>high))
-    mask=ma.make_mask_none(x.shape)
-    mask[:,colkill]=ones(mask[:,colkill].shape,dtype=bool)
+    iqrange = iqr(col_eff)
+    low = quantile(col_eff, 0.25) - cs * iqrange
+    high = quantile(col_eff, 0.75) + cs * iqrange
+    colkill = where((col_eff < low) | (col_eff > high))
+    mask = ma.make_mask_none(x.shape)
+    mask[:, colkill] = np.ones(mask[:, colkill].shape, dtype=bool)
     return mask
     
 def rcolclean(x, col_eff, cs):
-    iqrange=iqr(col_eff)
-    low=quantile(col_eff,0.25)-cs*iqrange
-    high=quantile(col_eff,0.75)+cs*iqrange
-    colkill=where((col_eff<low)|(col_eff>high))
-    mask=ma.make_mask_none(x.shape[1])
-    mask[colkill]=ones(mask[colkill].shape, dtype=bool)
+    iqrange = iqr(col_eff)
+    low = quantile(col_eff, 0.25) - cs * iqrange
+    high = quantile(col_eff, 0.75) + cs * iqrange
+    colkill = where((col_eff < low) | (col_eff > high))
+    mask = ma.make_mask_none(x.shape[1])
+    mask[colkill] = np.ones(mask[colkill].shape, dtype=bool)
+    return mask
+
+def seqclean(x, t):
+    contigs = np.apply_along_axis(longest_contig_max, 0, x)
+    colkill = where(contigs >= t)
+    mask = ma.make_mask_none(x.shape[1])
+    mask[:, colkill] = np.ones(mask[:, colkill].shape, dtype=bool)
     return mask
     
-##########################################################################
-# End colclean (column effect clean) function
-##########################################################################
+def fitclean(date, x, t):
+    
+    def covar(x, y):
+        '''Returns the covariance of x and y'''
+        cmat = cov(x, y)
+        return cmat[0, 1]
+        
+    def slope(y, x):
+        '''Returns the absolute value of the SLR slope'''
+        reg = vstack((ones(x.shape), x))
+        (beta, res, rank, singular) = linalg.lstsq(reg.transpose(), y)
+        return beta[1]
+    
+    # slopes = np.apply_along_axis(slope, 0, x, date)
+    
+    # using the var/cov formula for slope is faster than doing least squares
+    varx = var(date)
+    covxy = np.apply_along_axis(covar, 0, x, date)
+    slopes = abs(covxy / varx)
 
-##########################################################################
-# Begin resclean (residuals clean) function
-##########################################################################
+    colkill = where(slopes > t)
+    mask = ma.make_mask_none(x.shape[1])
+    mask[colkill] = ones(mask[colkill].shape, dtype=bool)
+    return mask
+
 def resclean(x, res, ce):
-    y=x.ravel()
-    iqrange=iqr(y)
-    low=quantile(y,0.25)-ce*iqrange
-    high=quantile(y,0.75)+ce*iqrange
-    mask=ma.masked_outside(x,low,high).mask
+    y = x.ravel()
+    iqrange = iqr(y)
+    low = quantile(y,0.25) - ce * iqrange
+    high = quantile(y, 0.75) + ce * iqrange
+    mask = ma.masked_outside(x, low, high).mask
     #if mask.dtype == np.dtype('bool'):
     #    mask = zeros(x.shape,dtype=bool)
     return mask
     
 def rresclean(x, lag, res, ce):
-    y=x.ravel()
-    iqrange=iqr(y)
-    low=quantile(y,0.25)-ce*iqrange
-    high=quantile(y,0.75)+ce*iqrange
-    mask=ma.masked_outside(x,low,high).mask
-    #if mask.dtype == np.dtype('bool'):
-    #    mask = zeros(x.shape,dtype=bool)
-    return mask[lag,:]
-    
-#http://automatthias.wordpress.com/2007/04/28/cartesian-product-of-multiple-sets/
+    y = x.ravel()
+    iqrange = iqr(y)
+    low = quantile(y, 0.25) - ce * iqrange
+    high = quantile(y, 0.75) + ce * iqrange
+    mask = ma.masked_outside(x, low, high).mask
+    if mask.dtype == np.dtype('bool'):
+        mask = zeros(x.shape, dtype=bool)
+    return mask[lag, :]
+
+# cprod comes from:
+# http://automatthias.wordpress.com/2007/04/28/cartesian-product-of-multiple-sets/
 
 def cprod(lists, previous_elements = []):
+    '''
+    Returns the Cartesian product of the elements in lists
+    '''
     if len(lists) == 1:
         for elem in lists[0]:
             yield previous_elements + [elem, ]
